27f9abe62a6bcc369063aadb57c550f46fd3515c
Chantel Gee <chmagee@ucsc.edu>
2021-01-24 13:47:24 -0800

    updated Makefile

--- pa2/Graph.h
+++ pa2/Graph.h
@@ -13,7 +13,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
 #include "List.h"
 
--- pa2/List.h
+++ pa2/List.h
@@ -13,7 +13,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 // Exported type --------------------------------------------------------------
 
